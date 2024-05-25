b8f199ff5a6f48e10065701cb23dfab405b4eec2
Ctvercik <ctvercik@gmail.com>
2024-05-25 18:49:46 +0200

    added some CSS

--- React/wa-app/src/components/Navbar.js
+++ React/wa-app/src/components/Navbar.js
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom"
 import "./styles.css"
-import React from 'react'
+import React, { useState } from 'react'
+import {FaBars,FaTimes}from "react-icons/fa";
 
 const Navbar = () => {
+const[click,setClick] = useState(false);
+const handleClick = () => setClick(!click)
+
   return (
     <div className="header">
       <Link to ="/">
         <h1>GVP</h1>
       </Link>
-       <ul className="navigation">
+       <ul className={click ? "navigation active" : "navigation"}>
           <li>
             <Link to ="/Articles">Articles</Link>
           </li>
@@ -19,6 +23,12 @@ const Navbar = () => {
             <Link to ="/Register">Sign in</Link>
           </li>
        </ul>
+       <div className="menuForSmallMon" onClick={handleClick}>
+        {click ? (<FaTimes size={20} style={{color: "rgb(224, 209, 89)"}} />)
+         : (<FaBars size={20} style={{color: "rgb(224, 209, 89)"}} />) }
+          
+          
+       </div>
     </div>
   )
 }
